51763bd7115f0ada467104a8c90b16f89a0ffcca
cho5butter <akutore@gmail.com>
2022-09-21 21:24:02 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsController/App.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WindowsController
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static System.Timers.Timer timer1;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            base.OnStartup(e);
+
+            var menu = new System.Windows.Forms.ContextMenuStrip();
+            menu.Items.Add("終了", null, Exit_Click);
+
+            var notifyIcon = new System.Windows.Forms.NotifyIcon
+            {
+                Visible = true,
+                Icon = new System.Drawing.Icon(@"favicon.ico"),
+                Text = "Windows Controller",
+                ContextMenuStrip = menu
+            };
+
+            notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(NotifyIcon_Click);
+
+            timer1 = new System.Timers.Timer(1000);
+            timer1.Start();
+
+            int x = System.Windows.Forms.Cursor.Position.X;
+            //Y座標を取得する
+            int y = System.Windows.Forms.Cursor.Position.Y;
+
+            //マウスポインタの位置を画面左上（座標 (0, 0)）にする
+            System.Windows.Forms.Cursor.Position = new System.Drawing.Point(0, 0);
+        }
+
+
+        private void NotifyIcon_Click(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                var wnd = new MainWindow();
+                wnd.Show();
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Shutdown();
+        }
+    }
+}
---
+++ WindowsController/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowsController
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IntPtr hHook;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Minimized;
+            this.Visibility = Visibility.Hidden;
+            this.SetHook();
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("hiik");
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("hiik");
+            if (e.Key == Key.Escape)
+            { Close(); }
+        }
+
+        private int SetHook()
+        {
+            IntPtr hmodule = WindowsAPI.GetModuleHandle(System.Diagnostics.Process.GetCurrentProcess().MainModule.ModuleName);
+
+            hHook = WindowsAPI.SetWindowsHookEx((int)WindowsAPI.HookType.WH_MOUSE_LL, (WindowsAPI.HOOKPROC)MyHookProc, hmodule, IntPtr.Zero);
+            if (hHook == null)
+            {
+                MessageBox.Show("SetWindowsHookEx 失敗", "Error");
+                return -1;
+            }
+            else
+            {
+                MessageBox.Show("SetWindowsHookEx 成功", "OK");
+                return 0;
+            }
+        }
+
+        private IntPtr MyHookProc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (0 == WindowsAPI.HC_ACTION)
+            {
+                WindowsAPI.MSLLHOOKSTRUCT MouseHookStruct = (WindowsAPI.MSLLHOOKSTRUCT)System.Runtime.InteropServices.Marshal.PtrToStructure(lParam, typeof(WindowsAPI.MSLLHOOKSTRUCT));
+                //MessageBox.Show(Convert.ToString(MouseHookStruct.pt.x));
+            }
+
+            return WindowsAPI.CallNextHookEx(hHook, nCode, wParam, lParam);
+
+        }
+
+    }
+}
