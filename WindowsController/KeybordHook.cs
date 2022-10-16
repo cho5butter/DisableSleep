04d4c4347771253b7eef4915bbcea635dc7c4881
cho5butter <akutore@gmail.com>
2022-10-16 23:08:39 +0900

    UPGRADE自動起動搭載・マウス・キーボード安定検知

---
+++ WindowsController/KeybordHook.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace WindowsController
+{
+    internal class KeybordHook
+    {
+    }
+    abstract class AbstractInterceptKeyboard
+    {
+        #region Win32 Constants
+        protected const int WH_KEYBOARD_LL = 0x000D;
+        protected const int WM_KEYDOWN = 0x0100;
+        protected const int WM_KEYUP = 0x0101;
+        protected const int WM_SYSKEYDOWN = 0x0104;
+        protected const int WM_SYSKEYUP = 0x0105;
+        #endregion
+
+        #region Win32API Structures
+        [StructLayout(LayoutKind.Sequential)]
+        public class KBDLLHOOKSTRUCT
+        {
+            public uint vkCode;
+            public uint scanCode;
+            public KBDLLHOOKSTRUCTFlags flags;
+            public uint time;
+            public UIntPtr dwExtraInfo;
+        }
+
+        [Flags]
+        public enum KBDLLHOOKSTRUCTFlags : uint
+        {
+            KEYEVENTF_EXTENDEDKEY = 0x0001,
+            KEYEVENTF_KEYUP = 0x0002,
+            KEYEVENTF_SCANCODE = 0x0008,
+            KEYEVENTF_UNICODE = 0x0004,
+        }
+        #endregion
+
+        #region Win32 Methods
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, KeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        #endregion
+
+        #region Delegate
+        private delegate IntPtr KeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        #endregion
+
+        #region Fields
+        private KeyboardProc proc;
+        private IntPtr hookId = IntPtr.Zero;
+        #endregion
+
+        public void Hook()
+        {
+            if (hookId == IntPtr.Zero)
+            {
+                proc = HookProcedure;
+                using (var curProcess = Process.GetCurrentProcess())
+                {
+                    using (ProcessModule curModule = curProcess.MainModule)
+                    {
+                        hookId = SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+                    }
+                }
+            }
+        }
+
+        public void UnHook()
+        {
+            UnhookWindowsHookEx(hookId);
+            hookId = IntPtr.Zero;
+        }
+
+        public virtual IntPtr HookProcedure(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            return CallNextHookEx(hookId, nCode, wParam, lParam);
+        }
+    }
+
+    class InterceptKeyboard : AbstractInterceptKeyboard
+    {
+        #region InputEvent
+        public class OriginalKeyEventArg : EventArgs
+        {
+            public int KeyCode { get; }
+
+            public OriginalKeyEventArg(int keyCode)
+            {
+                KeyCode = keyCode;
+            }
+        }
+        public delegate void KeyEventHandler(object sender, OriginalKeyEventArg e);
+        public event KeyEventHandler KeyDownEvent;
+        public event KeyEventHandler KeyUpEvent;
+
+        protected void OnKeyDownEvent(int keyCode)
+        {
+            KeyDownEvent?.Invoke(this, new OriginalKeyEventArg(keyCode));
+        }
+        protected void OnKeyUpEvent(int keyCode)
+        {
+            KeyUpEvent?.Invoke(this, new OriginalKeyEventArg(keyCode));
+        }
+        #endregion
+
+        public override IntPtr HookProcedure(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN))
+            {
+                var kb = (KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(KBDLLHOOKSTRUCT));
+                var vkCode = (int)kb.vkCode;
+                OnKeyDownEvent(vkCode);
+            }
+            else if (nCode >= 0 && (wParam == (IntPtr)WM_KEYUP || wParam == (IntPtr)WM_SYSKEYUP))
+            {
+                var kb = (KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(KBDLLHOOKSTRUCT));
+                var vkCode = (int)kb.vkCode;
+                OnKeyUpEvent(vkCode);
+            }
+
+            return base.HookProcedure(nCode, wParam, lParam);
+        }
+    }
+
+    class MouseHook
+    {
+        protected const int WH_MOUSE_LL = 14;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, MouseHookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        // これをしないとGCにより回収されてしまってCallbackOnCollectedDelegate例外で詰む
+        private delegate IntPtr MouseHookProc(int nCode, IntPtr wParam, IntPtr lParam);
+        MouseHookProc mouseHookProc = null;
+
+        private IntPtr hookId = IntPtr.Zero;
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MSLLHOOKSTRUCT
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public System.IntPtr dwExtraInfo;
+        }
+
+        public void Hook()
+        {
+            if (hookId == IntPtr.Zero)
+            {
+                using (var curProcess = Process.GetCurrentProcess())
+                {
+                    using (ProcessModule curModule = curProcess.MainModule)
+                    {
+                        // フィールド変数にHookProcedureを保存する
+                        // そうしないとGCにより回収されてしまってCallbackOnCollectedDelegate例外で詰む
+                        mouseHookProc = HookProcedure;
+                        hookId = SetWindowsHookEx(WH_MOUSE_LL, mouseHookProc, GetModuleHandle(curModule.ModuleName), 0);
+                    }
+                }
+            }
+        }
+
+        public void UnHook()
+        {
+            UnhookWindowsHookEx(hookId);
+            hookId = IntPtr.Zero;
+        }
+
+        public IntPtr HookProcedure(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            int WM_MOUSEMOVE = 0x0200;
+            int WM_LBUTTONDOWN = 0x0201;
+            int WM_LBUTTONUP = 0x0202;
+            int WM_RBUTTONDOWN = 0x0204;
+            int WM_RBUTTONUP = 0x0205;
+
+            if (nCode >= 0 && wParam == (IntPtr)WM_MOUSEMOVE)
+            {
+                MSLLHOOKSTRUCT ms = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                MouseMoveEvent?.Invoke(this, new MouseEventArg(ms.pt.x, ms.pt.y));
+            }
+            if (nCode >= 0 && wParam == (IntPtr)WM_LBUTTONDOWN)
+            {
+                MSLLHOOKSTRUCT ms = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                LButtonDownEvent?.Invoke(this, new MouseEventArg(ms.pt.x, ms.pt.y));
+            }
+            if (nCode >= 0 && wParam == (IntPtr)WM_LBUTTONUP)
+            {
+                MSLLHOOKSTRUCT ms = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                LButtonUpEvent?.Invoke(this, new MouseEventArg(ms.pt.x, ms.pt.y));
+            }
+            if (nCode >= 0 && wParam == (IntPtr)WM_RBUTTONDOWN)
+            {
+                MSLLHOOKSTRUCT ms = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                RButtonDownEvent?.Invoke(this, new MouseEventArg(ms.pt.x, ms.pt.y));
+            }
+            if (nCode >= 0 && wParam == (IntPtr)WM_RBUTTONUP)
+            {
+                MSLLHOOKSTRUCT ms = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                RButtonUpEvent?.Invoke(this, new MouseEventArg(ms.pt.x, ms.pt.y));
+            }
+
+            return CallNextHookEx(hookId, nCode, wParam, lParam);
+        }
+
+        public delegate void MouseEventHandler(object sender, MouseEventArg e);
+        public event MouseEventHandler MouseMoveEvent;
+        public event MouseEventHandler LButtonDownEvent;
+        public event MouseEventHandler LButtonUpEvent;
+        public event MouseEventHandler RButtonDownEvent;
+        public event MouseEventHandler RButtonUpEvent;
+    }
+
+    public class MouseEventArg : EventArgs
+    {
+        public MouseEventArg(int x, int y)
+        {
+            Point = new System.Drawing.Point(x, y); 
+        }
+        public System.Drawing.Point Point
+        {
+            get;
+            private set;
+        }
+    }
+
+}
